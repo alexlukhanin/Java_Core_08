313f1f85016db562466e6f39a57699a0dab1f6e8
Oleksandr Lukhanin <60075645+alexlukhanin@users.noreply.github.com>
2020-03-16 01:30:59 +0200

    first release

---
+++ hw08/src/hw08/Months.java
@@ -0,0 +1,22 @@
+package hw08;
+
+public enum Months {
+	JANUARY(31, Seasons.WINTER), FEBRUARY(28, Seasons.WINTER), MARCH(31, Seasons.SPRING), APRIL(30, Seasons.SPRING), MAY(31, Seasons.SPRING),
+	JUNE(30, Seasons.SUMMER), JULY(31, Seasons.SUMMER), AUGUST(31, Seasons.SUMMER), SEPTEMBER(30, Seasons.FALL), OCTOVBER(31, Seasons.FALL), 
+	NOVEMBER(30, Seasons.FALL), DECEMBER(31, Seasons.WINTER);
+	
+	private int days;
+	private Seasons seasons;
+	private Months(int days, Seasons seasons) {
+		this.days = days;
+		this.seasons = seasons;
+	}
+	
+	public int getDays() {
+		return days;
+	}
+	public Seasons getSeasons() {
+		return seasons;
+	}
+
+}
---
+++ hw08/src/hw08/Seasons.java
@@ -0,0 +1,5 @@
+package hw08;
+
+public enum Seasons {
+	WINTER, SPRING, SUMMER, FALL;
+}
